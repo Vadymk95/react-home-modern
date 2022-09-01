cddd48b2f2072191233f01319a9df03fae6e3b70
Vadym Kononenko <vadymk95@gmail.com>
2022-09-01 14:14:43 +0300

    master/fix/tailwind/added small fix with path

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { FC } from 'react';
 const App: FC = () => {
   return (
     <div className="bg-primary w-full overflow-hidden">
-      <h1 className="p-4">Hello world</h1>
+      <h1>Hello world</h1>
     </div>
   );
 };
