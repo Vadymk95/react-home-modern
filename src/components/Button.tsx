686c8862811c76865a4441a4ede8db1593dc98b5
Vadym Kononenko <vadymk95@gmail.com>
2022-09-04 17:34:24 +0300

    master/feat/CardDeal/created and styled component

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -8,7 +8,7 @@ export const Button: FC<IButtonProps> = ({ styles }) => {
   return (
     <button
       type="button"
-      className={`${styles} py-4 px-6 bg-blue-gradient font-poppins font-medium text-[18px] text-primary outline-none`}
+      className={`${styles} py-4 px-6 bg-blue-gradient font-poppins font-medium text-[18px] text-primary rounded-[10px] outline-none`}
     >
       Get Started
     </button>
--- src/components/CardDeal.tsx
+++ src/components/CardDeal.tsx
@@ -1,7 +1,26 @@
-import React, { FC } from 'react'
+import { FC } from 'react';
+import { card } from '../assets';
+import styles, { layout } from '../style';
+import { Button } from './Button';
 
 export const CardDeal: FC = () => {
   return (
-    <div>CardDeal</div>
-  )
-}
+    <section className={layout.section}>
+      <div className={layout.sectionInfo}>
+        <h2 className={styles.heading2}>
+          Find a better card deal <br className="sm:block hidden" /> in few easy
+          steps.
+        </h2>
+        <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatum
+          magni id sit sunt possimus, voluptas nulla delectus quos repellat
+          laboriosam!
+        </p>
+        <Button styles="mt-10" />
+      </div>
+      <div className={layout.sectionImg}>
+        <img src={card} alt="card" className="w-[100%] h-[100%] " />
+      </div>
+    </section>
+  );
+};
