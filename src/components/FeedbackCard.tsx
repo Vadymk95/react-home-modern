c6e7efc6c0f0616cc417a3a288cefef03717e003
Vadym Kononenko <vadymk95@gmail.com>
2022-09-04 18:14:55 +0300

    master/feat/added new components: Testimonials, Clients, FeedbackCard

--- src/components/FeedbackCard.tsx
+++ src/components/FeedbackCard.tsx
@@ -1,7 +1,32 @@
-import { FC } from 'react'
+import { FC } from 'react';
+import { quotes } from '../assets';
+import { IFeedback } from '../constants/models';
 
-export const FeedbackCard: FC = () => {
+interface IFeedbackCardProps extends IFeedback {}
+
+export const FeedbackCard: FC<IFeedbackCardProps> = ({
+  content,
+  name,
+  title,
+  img,
+}) => {
   return (
-    <div>FeedbackCard</div>
-  )
-}
+    <div className="flex justify-between flex-col px-10 py-12 rounded-[20px] max-w-[370px] md:mr-10 sm:mr-5 mr-0 my-5 feedback-card">
+      <img
+        src={quotes}
+        alt="quotes"
+        className="w-[42px] h-[27px] object-contain"
+      />
+      <p className="font-poppins font-normal text-[18px] leading-[32px] text-white my-10">
+        {content}
+      </p>
+      <div className="flex flex-row">
+        <img src={img} alt={name} className="w-[48px] h-48px rounded-full" />
+        <div className="flex flex-col ml-4">
+          <h4 className="font-poppins font-semibold text-[20px] leading-[32px] text-white">{name}</h4>
+          <p className="font-poppins font-normal text-[16px] leading-[24px] text-dimWhite">{title}</p>
+        </div>
+      </div>
+    </div>
+  );
+};
